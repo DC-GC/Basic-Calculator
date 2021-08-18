00880019f8d8664bf7f4eff08b64535537e27128
Dan Medilla <danmedilla@gmail.com>
2021-08-18 14:05:46 +0800

    removed 1 entrybox from calculator and answer are now shown in the entrybox

--- calculator.py
+++ calculator.py
@@ -1,39 +1,76 @@
 from tkinter import *
 
 
-def addition(a=0,b=0):
-    print(int(input2.get() or 0)+int(int(input1.get() or 0)))
-    # print(type(input2.get()))
-    
-def subtraction(a=0, b=0):
-    print(int(input2.get() or 0)-int(int(input1.get() or 0)))
+first_input = 0
+second_input = 0
+action = "none"
 
-def multiplication(a=0, b=0):
-    print(int(input2.get() or 0)*int(int(input1.get() or 0)))
 
-def division(a=0, b=0):
-    print(int(input2.get() or 0)/int(int(input1.get() or 1)))
-
-def clear():
+def addition():
+    global first_input
+    global action
+    first_input = int(input2.get() or 0)
+    action = "addition"
     input2.delete(0, END)
-    input1.delete(0, END)
+    
+    
+    
+def subtraction():
+    global first_input
+    global action
+    first_input = int(input2.get() or 0)
+    action = "subtraction"
+    input2.delete(0, END)
+
+def multiplication():
+    global first_input
+    global action
+    first_input = int(input2.get() or 0)
+    action = "multiplication"
+    input2.delete(0, END)
+
+def division():
+    global first_input
+    global action
+    first_input = int(input2.get() or 0)
+    action = "division"
+    input2.delete(0, END)
+
+def perform_action():
+    global action
+    global second_input
+    second_input = int(input2.get() or 0)
+    input2.delete(0, END)
+    if action == "addition":
+        input2.insert(0,first_input + second_input)
+    elif action == "subtraction":
+        input2.insert(0,first_input - second_input)
+    elif action == "multiplication":
+        input2.insert(0,first_input * second_input)
+    elif action == "division":
+        input2.insert(0,first_input / second_input)
+    
+    
+
+    
+    
 
 # addition(input("enter a number: ") or 0, input("enter another number: ") or 0)
 window = Tk()
 # window.geometry("500x500")
-window.title("GUI :)")
+window.title("Calculator")
 window.config(background="black")
 
 
 
 input2 = Entry(window, 
-            font=("Comic Sans", 10))
-input2.grid(row=0, column=0)
+            font=("Comic Sans", 15))
+input2.grid(row=0, column=0, columnspan=2)
 
 
-input1 = Entry(window, 
-            font=("Comic Sans", 10))
-input1.grid(row=0, column=1)
+# input1 = Entry(window, 
+#             font=("Comic Sans", 10))
+# input1.grid(row=0, column=1)
 
 plus = Button(window, 
                 text = "+", 
@@ -74,7 +111,7 @@ divide = Button(window,
 
 equal = Button(window, 
                 text = "=", 
-                command=clear, 
+                command=perform_action, 
                 font=("Comic Sans", 15), 
                 fg="blue",
                 bg="black",
